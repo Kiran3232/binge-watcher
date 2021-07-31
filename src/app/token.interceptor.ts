e368c3f87a1ac38479ac0e3aa7d4f02835ed492a
Kiran Verghese <kiran13rk@gmail.com>
2021-07-31 20:09:00 +0530

    Angular Update to 10

--- src/app/token.interceptor.ts
+++ src/app/token.interceptor.ts
@@ -1,7 +1,9 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class TokenInterceptor implements HttpInterceptor {
     intercept(
         req: HttpRequest<any>,
